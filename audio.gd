8ec63dff5a1df72f725e25075de9361943dad7c1
driftingdeluge <DriftingDeluge@gmail.com>
2026-04-18 11:05:57 -0700

    Add auio management

---
+++ audio.gd
@@ -0,0 +1,77 @@
+extends Node
+
+class PlayMessage:
+	var id
+	var volume
+
+func _process(delta):
+	update()
+
+func _ready():
+	# create the audio channels (8)
+	for i in range(CHANNEL_COUNT):
+		var player = AudioStreamPlayer.new()
+		add_child(player)
+		channels.append(player)
+	
+	# initialize queue for play messages
+	for i in range(MAX_PENDING):
+		pending_.append(PlayMessage.new())
+
+func playSound(id, volume):
+	var i = head_
+	
+	while i != tail_:
+		if pending_[i].id == id:
+			pending_[i].volume = max(volume, pending_[i].volume)
+			return
+		i = (i + 1) % MAX_PENDING
+	assert((tail_ + 1) % MAX_PENDING != head_)
+	
+	pending_[tail_].id = id
+	pending_[tail_].volume = volume
+	tail_ = (tail_ + 1) % MAX_PENDING
+
+func startSound(resource, channel, volume):
+	var player = channels[channel]
+	player.stream = resource
+	player.volume_db = volume
+	player.play()
+
+func findOpenChannel():
+	for i in range(channels.size()):
+		if not channels[i].playing:
+			return i
+	return -1
+
+#func update():
+	#if head_ == tail_:
+		#return
+	#var resource = loadSound(pending_[head_].id)
+	#var channel = findOpenChannel()
+	#if (channel == -1):
+		#return
+	#startSound(resource, channel, pending_[head_].volume)
+	#head_ = (head_ + 1) % MAX_PENDING
+
+func update():
+	while head_ != tail_:
+		var resource = loadSound(pending_[head_].id)
+		var channel = findOpenChannel()
+		if channel == -1:
+			break  # no free channel, stop
+		startSound(resource, channel, pending_[head_].volume)
+		head_ = (head_ + 1) % MAX_PENDING
+
+func loadSound(id):
+	var stream: AudioStream = load("res://audio/%s.ogg" % id)
+	return stream
+
+var channels: Array[AudioStreamPlayer] = []
+# size of request queue
+const MAX_PENDING = 16
+const CHANNEL_COUNT = 8
+var pending_: Array[PlayMessage] = []
+#var _num_pending = 0
+var head_ = 0
+var tail_ = 0
