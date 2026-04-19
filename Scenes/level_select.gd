be799bc701835e086171df0bb33bf3874e8b6080
Avo Papakhian <avopapakhian@Avos-MacBook-Air.local>
2026-04-18 17:33:44 -0700

    L1Update1

---
+++ Scenes/level_select.gd
@@ -0,0 +1,5 @@
+extends Control
+
+func _on_level_1_pressed() -> void:
+	GameManager.goto_scene("res://Scenes/level1.tscn")
+	pass 
---
+++ Scenes/player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+@export var speed := 350
+@export var jump_force := -425
+@export var gravity := 1000
+@export var rotation_speed = 3.0
+@export var mode = 0 #0 for jump, 1 for switch direction, 2 for attack
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	velocity.x = speed
+	
+	if Input.is_action_just_pressed("changeMode"):
+		mode += 1
+		if mode > 2:
+			mode = 0
+
+	if Input.is_action_just_pressed("jump"):
+		match mode:   
+			0:
+				if is_on_floor():
+					velocity = Vector2.UP.rotated(rotation) * rotation_speed
+					velocity.y = jump_force
+					AudioManager.playSound("jump.wav", 0)
+			1:
+				speed = -speed
+			2:
+				mode = 2 # attack goes here once enemies are created
+
+	move_and_slide()
